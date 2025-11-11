a7a33f3047370a3e7859d0ed2ef1946970c838b0
ALADETAN-IFE <famousfotune123@gmail.com>
2025-11-11 12:13:08 +0100

    fix: update button layout and text for better clarity in UpdatePopup component

--- components/UpdatePopup.tsx
+++ components/UpdatePopup.tsx
@@ -29,17 +29,17 @@ const UpdatePopup: React.FC<UpdatePopupProps> = ({
       <View className="flex-1 justify-center items-center bg-black/50">
         <View className="bg-black shadow-slate-400 p-6 rounded-lg shadow-lg">
           <Text className="text-lg mb-4 text-white">{message}</Text>
-          {updateAvailable ? (
-            <View className="w-fit bg-green-500 flex-row justify-between">
+          {!updateAvailable ? (
+            <View className="w-[50%] self-end gap-3 flex-row justify-between">
               <TouchableOpacity
-                onPress={handleUpdate}
-                className="bg-blue-500 rounded-full px-6 py-2"
+                onPress={onClose}
+                className="bg-blue-500 rounded-lg px-6 py-2"
               >
-                <Text className="text-white text-center">Update Now</Text>
+                <Text className="text-white text-center">Update Later</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 onPress={handleUpdate}
-                className="bg-blue-500 rounded-full px-6 py-2"
+                className="bg-blue-500 rounded-lg px-6 py-2"
               >
                 <Text className="text-white text-center">Update Now</Text>
               </TouchableOpacity>
